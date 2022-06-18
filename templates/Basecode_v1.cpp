2ce567d07d341c23bbb559801035da022e42a4b3
holywater2 <holywater@gist.ac.kr>
2022-06-18 16:55:32 +0900

    0618

---
+++ templates/Basecode_v1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>  
+#include <queue>
+#include <algorithm>
+#include <cstring>  // memset(a,0,sizeof(a));
+
+#define sf1(a)      cin  >> a;
+#define pf1(a)      cout << a << '\n';
+#define pf2(a,b)    cout << a << ' ' << b << '\n';
+#define pb(a)       push_back(a)
+#define iR(l,a,h)   (a >= l && a < h)
+#define all(v)      v.begin(),v.end()
+#define INF 987654321   
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int N, M;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cin >> N;
+}
