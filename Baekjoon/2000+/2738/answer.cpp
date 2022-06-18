8743daf9533af49b27ae2181af397d5fb6350387
holywater2 <holywater@gist.ac.kr>
2022-06-18 18:35:06 +0900

    0618 2738

---
+++ Baekjoon/2000+/2738/answer.cpp
@@ -0,0 +1,54 @@
+#define LOCAL
+
+#include <iostream>
+#include <vector>
+#include <string>  
+#include <queue>
+#include <algorithm>
+#include <cstring>  // memset(a,0,sizeof(a));
+
+#define sf1(a)      cin  >> a;
+#define pf1(a)      cout << a << '\n';
+#define pf2(a,b)    cout << a << ' ' << b << '\n';
+#define pb(a)       push_back(a)
+#define iR(l,a,h)   (a >= l && a < h)
+#define all(v)      v.begin(),v.end()
+#define INF 987654321   
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int N, M;
+
+int main(){
+   #ifdef LOCAL
+      freopen("data/data.txt", "r", stdin);
+   //    freopen("data/input.txt", "r", stdin);
+    freopen("data/output.txt", "w", stdout);
+   #endif
+   ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+   cin >> N >> M;
+   vector<vector<int>> v = vector<vector<int>>(N, vector<int>(M,0));
+   for(int i = 0; i < N; i++){
+      for(int j = 0; j < M; j++){
+         cin >> v[i][j];
+      }
+   }
+   int temp;
+   for(int i = 0; i < N; i++){
+      for(int j = 0; j < M; j++){
+         cin >> temp;
+         v[i][j] += temp;
+      }
+   }
+   for(int i = 0; i < N; i++){
+      for(int j = 0; j < M; j++){
+         cout << v[i][j] << ' ';
+      }
+      cout << '\n';
+   }
+}
