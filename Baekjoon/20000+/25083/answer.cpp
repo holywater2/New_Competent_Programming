d76e20391131b8af7e8aa3dfa3b0898e0a665f6e
holywater2 <holywater@gist.ac.kr>
2022-06-18 17:54:22 +0900

    25083

---
+++ Baekjoon/20000+/25083/answer.cpp
@@ -0,0 +1,35 @@
+#define LOCAL
+
+#include <iostream>
+#include <vector>
+#include <string>  
+#include <queue>
+#include <algorithm>
+#include <cstring>  // memset(a,0,sizeof(a));
+
+#define sf1(a)      cin  >> a;
+#define pf1(a)      cout << a << '\n';
+#define pf2(a,b)    cout << a << ' ' << b << '\n';
+#define pb(a)       push_back(a)
+#define iR(l,a,h)   (a >= l && a < h)
+#define all(v)      v.begin(),v.end()
+#define INF 987654321   
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int N, M;
+
+string a = "         ,r\'\"7\nr`-_   ,\'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |\n";
+
+int main(){
+    #ifdef LOCAL
+       freopen("data/output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    cout << a;
+}
