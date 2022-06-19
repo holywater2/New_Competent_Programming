f458292a90031fac86819b880895ddcc0f498afb
holywater2 <holywater@gist.ac.kr>
2022-06-19 20:51:40 +0900

    0619 2745

---
+++ Baekjoon/2000+/2744/answer.cpp
@@ -0,0 +1,47 @@
+#define LOCAL
+
+#include <iostream>
+#include <vector>
+#include <string>  
+#include <queue>
+#include <algorithm>
+#include <cstring>  // memset(a,0,sizeof(a));
+
+#define sf1(a)      cin  >> a;
+#define pf1(a)      cout << a << '\n';
+#define pf2(a,b)    cout << a << ' ' << b << '\n';
+#define pb(a)       push_back(a)
+#define iR(l,a,h)   (a >= l && a < h)
+#define all(v)      v.begin(),v.end()
+#define INF 987654321   
+
+typedef unsigned long long ull;
+
+using namespace std;
+
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int N, M;
+string str;
+
+int main(){
+   #ifdef LOCAL
+      freopen("data/data.txt", "r", stdin);
+      // freopen("data/input.txt", "r", stdin);
+   //  freopen("data/output.txt", "w", stdout);
+   #endif
+   ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+   cin >> str;
+   for(int i = 0; i < str.length(); i++){
+      if(str[i] <= 'z' && str[i] >= 'a'){
+         str[i] += 'A'-'a';
+      } else{
+         str[i] -= 'A'-'a';
+      }
+   }
+   for(int i = 0; i < str.length(); i++){
+      cout << str[i];      
+   }
+   cout << '\n';
+}
